8ed66acbcd3067fccf77bca94e77ffdcc16c1410
mckayc <mckayjunk@gmail.com>
2017-01-23 23:11:04 -0700

    Update SetupScript.sh

--- SetupScript.sh
+++ SetupScript.sh
@@ -5,8 +5,8 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-sudo add-apt-repository ppa:kdenlive/kdenlive-stable
-sudo add-apt-repository ppa:pmjdebruijn/darktable-release
+sudo add-apt-repository ppa:kdenlive/kdenlive-stable -y
+sudo add-apt-repository ppa:pmjdebruijn/darktable-release -y
 
 #Update all packages
 sudo apt-get update 
@@ -15,7 +15,19 @@ sudo apt-get upgrade
 #Remove Packages
 
 #Install Packages
-sudo apt-get install -y google-chrome-stable kdenlive darktable vlc synaptic nemo playonlinux gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape
+sudo apt-get install -y 
+google-chrome-stable 
+kdenlive 
+darktable 
+vlc 
+synaptic 
+nemo 
+playonlinux 
+gimp 
+gimp-data-extras 
+gimp-gmic 
+gimp-plugin-registry 
+inkscape
 
 
 
