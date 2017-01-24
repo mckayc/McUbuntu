0bf087cf8a553190d009ad3805e2c9833935de42
mckayc <mckayjunk@gmail.com>
2017-01-24 06:54:27 -0700

    Update SetupScript.sh

--- SetupScript.sh
+++ SetupScript.sh
@@ -7,30 +7,17 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 
 sudo add-apt-repository ppa:kdenlive/kdenlive-stable -y
 sudo add-apt-repository ppa:pmjdebruijn/darktable-release -y
+sudo add-apt-repository ppa:peterlevi/ppa -y
 
 #Update all packages
 sudo apt-get update 
 sudo apt-get upgrade
 
 #Remove Packages
+sudo apt-get remove rhythmbox transmission-gtk transmission-common totem totem-common totem-plugins
 
 #Install Packages
-sudo apt-get install -y 
-google-chrome-stable 
-kdenlive 
-darktable 
-vlc 
-synaptic 
-nemo 
-playonlinux 
-gimp 
-gimp-data-extras 
-gimp-gmic 
-gimp-plugin-registry 
-inkscape
-virtualbox
-virtualbox-ext-pack
-virtualbox-guest-additions-iso
+sudo apt-get install -y google-chrome-stable kdenlive darktable vlc synaptic nemo playonlinux gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso qbittorrent variety variety-slideshow blender audacity lmms openvpn network-manager-openvpn-gnome
 
 
 
