f4ee092eade95f273299d119faf9e4550b8daccb
mckayc <mckayjunk@gmail.com>
2017-02-18 15:35:44 -0700

    Update SetupScriptGnome.sh

--- SetupScriptGnome.sh
+++ SetupScriptGnome.sh
@@ -17,6 +17,7 @@ sudo add-apt-repository ppa:pmjdebruijn/darktable-release -y
 sudo add-apt-repository ppa:peterlevi/ppa -y
 sudo add-apt-repository ppa:daniel.pavel/solaar -y
 sudo add-apt-repository ppa:webupd8team/atom -y
+sudo add-apt-repository ppa:obsproject/obs-studio -y
 
 #Update all packages
 sudo apt-get update 
@@ -25,7 +26,7 @@ sudo apt-get update
 sudo apt-get purge -y rhythmbox transmission-gtk transmission-common totem totem-common totem-plugins amarok dragonplayer
 
 #Install Packages
-sudo apt-get install -y google-chrome-stable kdenlive kde-runtime darktable vlc synaptic nemo gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape qbittorrent variety variety-slideshow blender audacity lmms openvpn network-manager-openvpn-gnome solaar solaar-gnome3 atom playonlinux
+sudo apt-get install -y google-chrome-stable kdenlive kde-runtime darktable vlc synaptic nemo gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape qbittorrent variety variety-slideshow blender audacity lmms openvpn network-manager-openvpn-gnome solaar solaar-gnome3 atom playonlinux ffmpeg obs-studio
 
 #Upgrade all packages
 sudo apt-get upgrade -y
