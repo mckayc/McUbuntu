b07a726238406e79e913bae9ed3dc85d46fea9d6
mckayc <mckayjunk@gmail.com>
2017-07-07 20:07:39 -0600

    Update and rename SetupScriptGnome.sh to UbuntuSetupScript.sh

--- UbuntuSetupScript.sh
+++ UbuntuSetupScript.sh
@@ -25,7 +25,7 @@ sudo apt-get update
 sudo apt-get purge -y rhythmbox transmission-gtk transmission-common totem totem-common totem-plugins amarok dragonplayer
 
 #Install Packages
-sudo apt-get install -y google-chrome-stable kdenlive darktable vlc synaptic gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape qbittorrent blender audacity lmms openvpn solaar playonlinux ffmpeg obs-studio mirage shutter ubuntu-make
+sudo apt-get install -y google-chrome-stable kdenlive darktable vlc synaptic gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape qbittorrent blender audacity lmms openvpn network-manager-openvpn solaar playonlinux ffmpeg obs-studio mirage shutter ubuntu-make
 
 #Upgrade all packages
 sudo apt-get upgrade -y
