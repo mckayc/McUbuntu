a841cea94d4727bccc5feba08e62185864816805
mckayc <mckayjunk@gmail.com>
2017-09-27 15:18:27 -0600

    Update SlimUbuntu.sh

--- SlimUbuntu.sh
+++ SlimUbuntu.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Slim Ubuntu by removing packages not needed.
 
+#Type the following commands in the terminal to run:
+#- - - - - - - - - - - - - - - - - - - -
+#wget http://bit.ly/lucidubu
+#chmod +x lucidubu
+#sudo ./lucidubu
+#- - - - - - - - - - - - - - - - - - - -
+
 #Update all packages
 sudo apt-get update 
 
