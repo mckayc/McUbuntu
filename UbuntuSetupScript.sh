f5b086fd2c9c64812049bc4a5c0b3ec66d876dca
mckayc <mckayjunk@gmail.com>
2019-04-06 07:16:01 -0600

    Update UbuntuSetupScript.sh

--- UbuntuSetupScript.sh
+++ UbuntuSetupScript.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 # Setup script for installing all my favorite software.
 
+#-------------------------------------------------------
+#---------------------INSTRUCTIONS----------------------
+#Install Google Chrome manually so that you have a browser to setup while everything else is downloading
+# - - - - - 
+#In terminal type/paste in the following command:
+# wget http://bit.ly/mckayubuntu ; chmod +x mckayubuntu ; ./mckayubuntu
+#-------------------------------------------------------
+
+
 #Add PPA Repositories
-
-# Install Google Chrome - Do this manually. This way you have a browser while the script is running.
-
-# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-# sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-# google-chrome-stable
-
-# Install Chromium (note: Spotify/Flash does not work currently in Chromium)
-# chromium-browser chrome-gnome-shell 
 
 # Install Etcher (USB Disk Creation Software)
 #echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/#sources.list.d/etcher.list
 #sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
-
-# Copy over Custom shortcuts
-wget https://raw.githubusercontent.com/mckayc/McUbuntu/master/McGlobalShortcuts.kksrc
-
-cp ~/.kde/share/config/kdeglobals ~/.kde/share/config/kdeglobals.bak
-mv McGlobalShortcuts.kksrc ~/.kde/share/config/kdeglobals
 
 # Add additional Repositories
 
@@ -32,6 +26,7 @@ sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo add-apt-repository ppa:gezakovacs/ppa -y
 #WoeUSB - Create live USBs for Windows
 sudo add-apt-repository ppa:nilarimogard/webupd8 -y
+
 
 #sudo add-apt-repository ppa:graphics-drivers/ppa -y
 #sudo add-apt-repository ppa:pmjdebruijn/darktable-release -y
@@ -48,13 +43,10 @@ sudo apt-get update
 sudo apt-get purge -y rhythmbox transmission-gtk transmission-common totem totem-common totem-plugins amarok dragonplayer
 
 #Install Packages
-sudo apt-get install -y kdenlive darktable vlc synaptic gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape qbittorrent blender audacity lmms openvpn network-manager-openvpn solaar playonlinux ffmpeg obs-studio mirage hydrogen qjackctl exfat-utils gparted rar unrar-free woeusb vlc-plugin-fluidsynth fluid-soundfont-gs fluid-soundfont-gm speedcrunch handbrake freecad unetbootin
+sudo apt-get install -y kdenlive darktable vlc synaptic gimp gimp-data-extras gimp-gmic gimp-plugin-registry inkscape qbittorrent blender audacity lmms openvpn network-manager-openvpn solaar playonlinux ffmpeg obs-studio mirage hydrogen qjackctl exfat-utils gparted rar unrar-free woeusb vlc-plugin-fluidsynth fluid-soundfont-gs fluid-soundfont-gm speedcrunch handbrake freecad unetbootin ubuntu-make
 
 #Not working for some reason:
 # sudo apt-get install -y shutter etcher-electron youtube-dlg
-
-#Install Latte-Dock
-#sudo apt-get install -y ubuntu-make latte-dock
 
 #Upgrade all packages
 sudo apt-get upgrade -y
@@ -69,3 +61,23 @@ sudo apt-get install -y virtualbox virtualbox-ext-pack virtualbox-guest-addition
 
 #Install VS Code via Umake
 sudo umake web visual-studio-code
+
+#------------------------------------------
+#System Tweaks
+
+#Increase Swap File Size to 16GB
+sudo swapoff /swapfile
+sudo rm  /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1M count=16384
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# Copy over KDE Custom shortcuts
+wget https://raw.githubusercontent.com/mckayc/McUbuntu/master/McGlobalShortcuts.kksrc
+
+cp ~/.kde/share/config/kdeglobals ~/.kde/share/config/kdeglobals.bak
+mv McGlobalShortcuts.kksrc ~/.kde/share/config/kdeglobals
+
+# Copy over Kdenlive effects
+
