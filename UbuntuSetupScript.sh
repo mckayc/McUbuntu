8ec186624a22ad37cb6add5dda8bda3a39d6ac50
McKay <mckay@lucidchart.com>
2021-08-06 15:14:07 -0600

    Take out parentasis

--- UbuntuSetupScript.sh
+++ UbuntuSetupScript.sh
@@ -17,7 +17,7 @@ read chrome
 ppa=n
 # - - - - End User Input Section - - - - - -
 
-if [$chrome = "y"]
+if [$chrome = y]
     then
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
