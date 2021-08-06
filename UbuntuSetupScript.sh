1705646e3a790cf420b0d220c0d86310b6ba3551
McKay <mckay@lucidchart.com>
2021-08-06 15:19:27 -0600

    confused

--- UbuntuSetupScript.sh
+++ UbuntuSetupScript.sh
@@ -19,6 +19,7 @@ ppa=n
 
 if ["$chrome" = "y"]
     then
+    echo "Installing Google Chrome."
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
     sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     sudo apt-get update
